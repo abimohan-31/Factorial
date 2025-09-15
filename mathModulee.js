1598a4912d95ddaff70ca0bc529e75e6ce38be45
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-16 05:17:20 +0530

    deleted

--- mathModulee.js
+++
@@ -1,9 +0,0 @@
- export function factorial(num) {
-        if (num === 0 || num === 1) {
-          return 1;
-        } else if (num < 0) {
-          alert("Please enter a valid number");
-        } else {
-          return num * factorial(num - 1);
-        }
-      }
