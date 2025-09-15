a6874e2ee483948912c6bb411a9add310bee8688
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-16 05:16:32 +0530

    import mathModule.js file

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+import { factorial } from "./mathModule.js";
+
 function calc(event) {
-        event.preventDefault();
-        const num = document.getElementById("num").value;
+  event.preventDefault();
+  const num = document.getElementById("num").value;
 
-        const result = factorial(num);
-        alert(result);
-        console.log(result);
-      
+  const result = factorial(num);
+//   alert(result);
+//   console.log(result);
 
-      document.getElementById("result").value = result;
+  document.getElementById("result").value = result;
+}
 
-      }
+const form = document.getElementById('facto-form');
+form.addEventListener('submit', calc);
